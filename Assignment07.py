01655ba488530ee8af1abbeca057588a9b632bee
greeneyedsoandso <27257805+greeneyedsoandso@users.noreply.github.com>
2020-03-04 21:33:10 -0800

    added example code from textbook

--- Assignment07.py
+++ Assignment07.py
@@ -5,13 +5,83 @@
 #              Demonstrate how structured error handling works
 # ChangeLog (Who,When,What):
 # JDSmith,3.2.20,Created file
+# JDSmith,3.4.20,Seeded with sample code
 # ---------------------------------------------------------------------------- #
 import pickle
 # TODO: pickle.dump, pickle.load
+print("Pickling lists.")
+variety = ["sweet", "hot", "dill"]
+shape = ["whole", "spear", "chip"]
+brand = ["Claussen", "Heinz", "Vlassic"]
+f = open("pickles1.dat", "wb")
+pickle.dump(variety, f)
+pickle.dump(shape, f)
+pickle.dump(brand, f)
+f.close()
+
+print("\nUnpickling lists.")
+f = open("pickles1.dat", "rb")
+variety = pickle.load(f)
+shape = pickle.load(f)
+brand = pickle.load(f)
+print(variety)
+print(shape)
+print(brand)
+f.close()
 # TODO: try exceptions
+# Handle It
+# Demonstrates handling exceptions
+
+# try/except
+try:
+    num = float(input("Enter a number: "))
+except:
+    print("Something went wrong!")
+
+# specifying exception type
+try:
+    num = float(input("\nEnter a number: "))
+except ValueError:
+    print("That was not a number!")
+
+# handle multiple exception types
+print()
+for value in (None, "Hi!"):
+    try:
+        print("Attempting to convert", value, "-->", end=" ")
+        print(float(value))
+    except (TypeError, ValueError):
+        print("Something went wrong!")
+
+print()
+for value in (None, "Hi!"):
+    try:
+        print("Attempting to convert", value, "-->", end=" ")
+        print(float(value))
+    except TypeError:
+        print("I can only convert a string or a number!")
+    except ValueError:
+        print("I can only convert a string of digits!")
+
+# get an exception's argument
+try:
+    num = float(input("\nEnter a number: "))
+except ValueError as e:
+    print("That was not a number! Or as Python would say...")
+    print(e)
+
+# try/except/else
+try:
+    num = float(input("\nEnter a number: "))
+except ValueError:
+    print("That was not a number!")
+else:
+    print("You entered the number", num)
+
+input("\n\nPress the enter key to exit.")
 
 # Data -------------------------------------------- #
 
 # Processing -------------------------------------- #
-pickle.
+
 # Presentation ------------------------------------ #
