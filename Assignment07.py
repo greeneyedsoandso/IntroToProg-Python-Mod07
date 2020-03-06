041439dae59056a47dc012b9d5cf81eae466c848
greeneyedsoandso <27257805+greeneyedsoandso@users.noreply.github.com>
2020-03-05 22:34:24 -0800

    Turning example code into something more evident to an end user.

--- Assignment07.py
+++ Assignment07.py
@@ -9,76 +9,79 @@
 # ---------------------------------------------------------------------------- #
 import pickle
 # TODO: pickle.dump, pickle.load
-print("Pickling lists.")
-variety = ["sweet", "hot", "dill"]
-shape = ["whole", "spear", "chip"]
-brand = ["Claussen", "Heinz", "Vlassic"]
-f = open("pickles1.dat", "wb")
-pickle.dump(variety, f)
-pickle.dump(shape, f)
-pickle.dump(brand, f)
-f.close()
+color1 = input('give me a color: ')
+color2 = input('give me a second color: ')
+color3 = input('give me a third color: ')
+shape1 = input('give me a shape: ')
+shape2 = input('give me a second shape: ')
+shape3 = input('give me a third shape: ')
+color = [color1, color2, color3]
+shape = [shape1, shape2, shape3]
+print("Pickling two lists into a binary file")
+file = open("shapes.dat", "wb")
+pickle.dump(color, file)
+pickle.dump(shape, file)
+file.close()
 
 print("\nUnpickling lists.")
-f = open("pickles1.dat", "rb")
-variety = pickle.load(f)
-shape = pickle.load(f)
-brand = pickle.load(f)
-print(variety)
+file = open("shapes.dat", "rb")
+color = pickle.load(file)
+shape = pickle.load(file)
+print(color)
 print(shape)
-print(brand)
-f.close()
+
+file.close()
 # TODO: try exceptions
 # Handle It
 # Demonstrates handling exceptions
 
 # try/except
-try:
-    num = float(input("Enter a number: "))
-except:
-    print("Something went wrong!")
-
-# specifying exception type
-try:
-    num = float(input("\nEnter a number: "))
-except ValueError:
-    print("That was not a number!")
-
-# handle multiple exception types
-print()
-for value in (None, "Hi!"):
-    try:
-        print("Attempting to convert", value, "-->", end=" ")
-        print(float(value))
-    except (TypeError, ValueError):
-        print("Something went wrong!")
-
-print()
-for value in (None, "Hi!"):
-    try:
-        print("Attempting to convert", value, "-->", end=" ")
-        print(float(value))
-    except TypeError:
-        print("I can only convert a string or a number!")
-    except ValueError:
-        print("I can only convert a string of digits!")
-
-# get an exception's argument
-try:
-    num = float(input("\nEnter a number: "))
-except ValueError as e:
-    print("That was not a number! Or as Python would say...")
-    print(e)
-
-# try/except/else
-try:
-    num = float(input("\nEnter a number: "))
-except ValueError:
-    print("That was not a number!")
-else:
-    print("You entered the number", num)
-
-input("\n\nPress the enter key to exit.")
+# try:
+#     num = float(input("Enter a number: "))
+# except:
+#     print("Something went wrong!")
+#
+# # specifying exception type
+# try:
+#     num = float(input("\nEnter a number: "))
+# except ValueError:
+#     print("That was not a number!")
+#
+# # handle multiple exception types
+# print()
+# for value in (None, "Hi!"):
+#     try:
+#         print("Attempting to convert", value, "-->", end=" ")
+#         print(float(value))
+#     except (TypeError, ValueError):
+#         print("Something went wrong!")
+#
+# print()
+# for value in (None, "Hi!"):
+#     try:
+#         print("Attempting to convert", value, "-->", end=" ")
+#         print(float(value))
+#     except TypeError:
+#         print("I can only convert a string or a number!")
+#     except ValueError:
+#         print("I can only convert a string of digits!")
+#
+# # get an exception's argument
+# try:
+#     num = float(input("\nEnter a number: "))
+# except ValueError as e:
+#     print("That was not a number! Or as Python would say...")
+#     print(e)
+#
+# # try/except/else
+# try:
+#     num = float(input("\nEnter a number: "))
+# except ValueError:
+#     print("That was not a number!")
+# else:
+#     print("You entered the number", num)
+#
+# input("\n\nPress the enter key to exit.")
 
 # Data -------------------------------------------- #
 
