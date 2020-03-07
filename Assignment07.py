450ddef8d24bcc702c33e4a91bf5fe53de8e6a4e
greeneyedsoandso <27257805+greeneyedsoandso@users.noreply.github.com>
2020-03-06 18:12:56 -0800

    Exceptions

--- Assignment07.py
+++ Assignment07.py
@@ -7,87 +7,69 @@
 # JDSmith,3.2.20,Created file
 # JDSmith,3.4.20,Seeded with sample code
 # JDSmith,3.5.20,Pickling demo
+# JDSmith,3.6.20,Exception handling
 # ---------------------------------------------------------------------------- #
-# import pickle
-#
-# print("Let's make a list to pickle.")
-# color1 = input('Give me a color: ')
-# color2 = input('Give me a second color: ')
-# color3 = input('Give me a third color: ')
-# shape1 = input('Give me a shape: ')
-# shape2 = input('Give me a second shape: ')
-# shape3 = input('Give me a third shape: ')
-# color = [color1, color2, color3]
-# shape = [shape1, shape2, shape3]
-# print("Pickling two lists into a binary file, shapes.dat...")
-# file = open("shapes.dat", "wb")
-# pickle.dump(color, file)
-# pickle.dump(shape, file)
-# file.close()
-# print("If you go open shapes.dat, it looks like junk inside. That's because it's not meant for humans to read,"
-#       " just computers.")
-# print("\nUnpickling lists from shapes.dat...")
-# file = open("shapes.dat", "rb")
-# color = pickle.load(file)
-# shape = pickle.load(file)
-# file.close()
-# print('Here is the information that was pickled.')
-# print(color)
-# print(shape)
-# print('The information that was pickled is now a list in memory, and you can mix it up however you like:')
-# print(color[0], shape[2] + ', ' + color[2], shape[1] + ', ' + color[1], shape[0])
-# print("That's pickling, another way to write and read data.\n")
-# # TODO: try exceptions
-# print('#' * 40 + '\n')
-# # Demonstrate handling exceptions
-# # try/except
-# print('We can deal with errors. Built in stuff from Python, and stuff we can make up.')
-# specifying exception type
+import pickle
+
+print("Let's make a list to pickle.")
+color1 = input('Give me a color: ')
+color2 = input('Give me a second color: ')
+color3 = input('Give me a third color: ')
+shape1 = input('Give me a shape: ')
+shape2 = input('Give me a second shape: ')
+shape3 = input('Give me a third shape: ')
+color = [color1, color2, color3]
+shape = [shape1, shape2, shape3]
+print("Pickling two lists into a binary file, shapes.dat...")
+file = open("shapes.dat", "wb")
+pickle.dump(color, file)
+pickle.dump(shape, file)
+file.close()
+print("If you go open shapes.dat, it looks like junk inside. That's because it's not meant for humans to read,"
+      " just computers.")
+print("\nUnpickling lists from shapes.dat...")
+file = open("shapes.dat", "rb")
+color = pickle.load(file)
+shape = pickle.load(file)
+file.close()
+print('Here is the information that was pickled.')
+print(color)
+print(shape)
+print('The information that was pickled is now a list in memory, and you can mix it up however you like:')
+print(color[0], shape[2] + ', ' + color[2], shape[1] + ', ' + color[1], shape[0])
+print("That's pickling, another way to write and read data.\n")
+print('#' * 40 + '\n')
+# Demonstrate handling exceptions
+# try/except
+print('We can deal with errors. Python has built in exceptions, '
+      'and an Exception class we can use with custom arguments.')
+print('There are many standard exceptions in Python, which you can find here: '
+      'https://docs.python.org/3/library/exceptions.html#bltin-exceptions')
+print('This example uses the built in ValueError.')
 try:
     num = float(input("\nEnter a number: "))
 except ValueError:
     print("That was not a number!")
 else:
-    print("Good job, you entered a number. Next time, try typing in a word instead, to see an error.")
-#
-# # handle multiple exception types
-# print()
-# for value in (None, "Hi!"):
-#     try:
-#         print("Attempting to convert", value, "-->", end=" ")
-#         print(float(value))
-#     except (TypeError, ValueError):
-#         print("Something went wrong!")
-#
-# print()
-# for value in (None, "Hi!"):
-#     try:
-#         print("Attempting to convert", value, "-->", end=" ")
-#         print(float(value))
-#     except TypeError:
-#         print("I can only convert a string or a number!")
-#     except ValueError:
-#         print("I can only convert a string of digits!")
-#
-# # get an exception's argument
-# try:
-#     num = float(input("\nEnter a number: "))
-# except ValueError as e:
-#     print("That was not a number! Or as Python would say...")
-#     print(e)
-#
-# # try/except/else
-# try:
-#     num = float(input("\nEnter a number: "))
-# except ValueError:
-#     print("That was not a number!")
-# else:
-#     print("You entered the number", num)
-#
-# input("\n\nPress the enter key to exit.")
+    print("Good job, you entered a number. Next time, try typing in a word instead to see an error.")
 
-# Data -------------------------------------------- #
+try:
+    num = float(input("\nEnter a number: "))
+except ValueError:
+    print("That was not a number!")
+else:
+    print("You entered a number, so you won't see the error.\n")
 
-# Processing -------------------------------------- #
+print('At the next prompt, try entering some words to see the use of the Exception class that is built into Python.\n')
+try:
+    everything = input("What is the meaning of Life, the Universe, and Everything? ")
+    if everything.isnumeric():
+        print("\nDirections were ignored. Let's pretend you typed some words.\n")
+        raise Exception('Something went wrong.')
+    else:
+        raise Exception('Maybe you should ask Deep Thought.')
+except Exception as e:
+    print(e)
+    print("\nException output: ")
+    print(f'The custom Exception text is "{e}"\nThe docstring for the Exception class is "{e.__doc__}"\nAnd the type is {type(e)}')
 
-# Presentation ------------------------------------ #
